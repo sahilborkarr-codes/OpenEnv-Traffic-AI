82e09556e0e1208ec96cc6151fd68d4fe9502a63
sahilborkarr-codes <sahilborkarr@gmail.com>
2026-04-09 11:20:17 +0530

    Update test_env.py

--- test_env.py
+++ test_env.py
@@ -9,9 +9,9 @@ print("Initial State:", state)
 for i in range(10):
     action = random.choice([0, 1, 2])
     state, reward, done, _ = env.step(action)
-    
-    print(f"Step {i+1}:")
+
+    print(f"Step {i+1}")
     print("Action:", action)
     print("State:", state)
     print("Reward:", reward)
-    print("-----------")
+    print("------")
